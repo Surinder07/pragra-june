427a02c59af2d10c63a16ebb83c294e72815d5bd
navjotsb <navjot2bhatia@gmail.com>
2024-06-13 21:27:15 -0400

    new commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("My Name is Navjot");
+        System.out.println("My Last name is Bhatia");
     }
 }
 
--- src/nav.java
+++
@@ -1,5 +0,0 @@
-public class nav {
-    public static void main(String[] args) {
-        System.out.println("Hello Team");
-    }
-}
