cdebb1df0b8d16e12a37fca77e209eb5cf06943c
surinder0322@gmail.com <sunny@sunnys-laptop.lan>
2024-06-13 21:01:01 -0400

    added some lines

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,11 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello this is Java");
+        System.out.println("Surinder");
+        System.out.println("Surinder");
+        System.out.println("Surinder");
+        System.out.println("Surinder");
+        System.out.println("Surinder");
+        System.out.println("Surinder");
     }
 }
